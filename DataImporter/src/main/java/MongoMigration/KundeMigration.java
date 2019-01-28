0486da344d53277cab60c772fd74aa4fe0127c9f
van15h <vansolo@posteo.net>
2019-01-28 10:58:08 +0100

    mitarbeiter migration

--- DataImporter/src/main/java/MongoMigration/KundeMigration.java
+++ DataImporter/src/main/java/MongoMigration/KundeMigration.java
@@ -3,7 +3,6 @@ package MongoMigration;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.oracle.javafx.jmx.json.JSONFactory;
 import org.bson.Document;
 
 import java.sql.Connection;
@@ -19,22 +18,52 @@ public class KundeMigration extends Migration {
 
     @Override
     public void fetch() {
+        // migrate to kunde
         try (Statement statement = this.connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Kunde INNER JOIN Person P ON Kunde.personid = P.id");
 
             while (resultSet.next()) {
-                String name = resultSet.getString("name");
                 Document document = new Document();
+                document.append("kundennummer", resultSet.getString("kundenummer"));
+                document.append("kontodaten", resultSet.getString("kontodaten"));
                 document.append("name", resultSet.getString("name"));
-                document.append("SVNummer", resultSet.getString("SVNummer"));
+                document.append("sv_nummer", resultSet.getString("SVNummer"));
                 document.append("geburtsdatum", resultSet.getString("geburtsdatum"));
-                document.append("kundenNummer", resultSet.getString("kundenummer"));
-                document.append("kontoDaten", resultSet.getString("kontodaten"));
-                document.append("telefonNummer", resultSet.getString("telefonnummer"));
-
-                System.out.println(name);
+                document.append("telefonnummer", resultSet.getString("telefonnummer"));
+                document.append("email", resultSet.getString("email"));
+                document.append("anschrift",
+                    new Document("ort", resultSet.getString("ort"))
+                        .append("plz", resultSet.getString("plz"))
+                        .append("strasse", resultSet.getString("strasse"))
+                );
 
                 MongoCollection<Document> collection = this.database.getCollection("Kunde");
+                collection.insertOne(document);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // migrate to mitarbeiter
+        try (Statement statement = this.connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Mitarbeiter INNER JOIN Person P ON Mitarbeiter.personid = P.id");
+
+            while (resultSet.next()) {
+                Document document = new Document();
+                document.append("steuernummer", resultSet.getString("steuernummer"));
+                document.append("gehalt", resultSet.getString("gehalt"));
+                document.append("name", resultSet.getString("name"));
+                document.append("svnummer", resultSet.getString("SVNummer"));
+                document.append("geburtsdatum", resultSet.getString("geburtsdatum"));
+                document.append("email", resultSet.getString("email"));
+                document.append("anschrift",
+                    new Document("ort", resultSet.getString("ort"))
+                        .append("plz", resultSet.getString("plz"))
+                        .append("strasse", resultSet.getString("strasse"))
+                );
+
+                MongoCollection<Document> collection = this.database.getCollection("Mitarbeiter");
                 collection.insertOne(document);
             }
 
