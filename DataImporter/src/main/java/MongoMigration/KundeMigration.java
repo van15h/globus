ba15d5bb2e5d8834008e055cf4f112cb871235f1
van15h <vansolo@posteo.net>
2019-01-28 13:42:42 +0100

    migration of main tables without arrays

--- DataImporter/src/main/java/MongoMigration/KundeMigration.java
+++ DataImporter/src/main/java/MongoMigration/KundeMigration.java
@@ -19,51 +19,120 @@ public class KundeMigration extends Migration {
     @Override
     public void fetch() {
         // migrate to kunde
+//        try (Statement statement = this.connection.createStatement()) {
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM Kunde INNER JOIN Person P ON Kunde.personid = P.id");
+//
+//            while (resultSet.next()) {
+//                Document document = new Document();
+//                document.append("personid", resultSet.getString("personid"));
+//                document.append("kundennummer", resultSet.getString("kundenummer"));
+//                document.append("kontodaten", resultSet.getString("kontodaten"));
+//                document.append("name", resultSet.getString("name"));
+//                document.append("sv_nummer", resultSet.getString("SVNummer"));
+//                document.append("geburtsdatum", resultSet.getString("geburtsdatum"));
+//                document.append("telefonnummer", resultSet.getString("telefonnummer"));
+//                document.append("email", resultSet.getString("email"));
+//                document.append("anschrift",
+//                    new Document("ort", resultSet.getString("ort"))
+//                        .append("plz", resultSet.getString("plz"))
+//                        .append("strasse", resultSet.getString("strasse"))
+//                );
+//
+//                MongoCollection<Document> collection = this.database.getCollection("Kunde");
+//                collection.insertOne(document);
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // migrate to mitarbeiter
+//        try (Statement statement = this.connection.createStatement()) {
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM Mitarbeiter INNER JOIN Person P ON Mitarbeiter.personid = P.id");
+//
+//            while (resultSet.next()) {
+//                Document document = new Document();
+//                document.append("personid", resultSet.getString("personid"));
+//                document.append("steuernummer", resultSet.getString("steuernummer"));
+//                document.append("gehalt", resultSet.getString("gehalt"));
+//                document.append("name", resultSet.getString("name"));
+//                document.append("svnummer", resultSet.getString("SVNummer"));
+//                document.append("geburtsdatum", resultSet.getString("geburtsdatum"));
+//                document.append("email", resultSet.getString("email"));
+//                document.append("reisebuero", resultSet.getString("beschaeftigungRBid"));
+//                document.append("anschrift",
+//                    new Document("ort", resultSet.getString("ort"))
+//                        .append("plz", resultSet.getString("plz"))
+//                        .append("strasse", resultSet.getString("strasse"))
+//                );
+//
+//                MongoCollection<Document> collection = this.database.getCollection("Mitarbeiter");
+//                collection.insertOne(document);
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+        //migrate reisebuero
+//        try (Statement statement = this.connection.createStatement()) {
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM Reisebuero");
+//
+//            while (resultSet.next()) {
+//                Document document = new Document();
+//                document.append("rbid", resultSet.getString("id"));
+//                document.append("name", resultSet.getString("name"));
+//                document.append("kontodaten", resultSet.getString("kontodaten"));
+//                document.append("anschrift",
+//                    new Document("ort", resultSet.getString("ort"))
+//                        .append("plz", resultSet.getString("plz"))
+//                        .append("strasse", resultSet.getString("strasse"))
+//                );
+//
+//                MongoCollection<Document> collection = this.database.getCollection("Reisebuero");
+//                collection.insertOne(document);
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+        // migrate reise
+//        try (Statement statement = this.connection.createStatement()) {
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM Reise");
+//
+//            while (resultSet.next()) {
+//                Document document = new Document();
+//                document.append("reiseid", resultSet.getString("id"));
+//                document.append("name", resultSet.getString("name"));
+//                document.append("einreisedatum", resultSet.getString("einreisedatum"));
+//                document.append("reisedauer", resultSet.getString("reisedauer"));
+//                document.append("preis", resultSet.getString("preis"));
+//
+//                MongoCollection<Document> collection = this.database.getCollection("Reise");
+//                collection.insertOne(document);
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
         try (Statement statement = this.connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Kunde INNER JOIN Person P ON Kunde.personid = P.id");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Hotel");
 
             while (resultSet.next()) {
                 Document document = new Document();
-                document.append("kundennummer", resultSet.getString("kundenummer"));
-                document.append("kontodaten", resultSet.getString("kontodaten"));
+                document.append("hotelid", resultSet.getString("id"));
                 document.append("name", resultSet.getString("name"));
-                document.append("sv_nummer", resultSet.getString("SVNummer"));
-                document.append("geburtsdatum", resultSet.getString("geburtsdatum"));
-                document.append("telefonnummer", resultSet.getString("telefonnummer"));
-                document.append("email", resultSet.getString("email"));
+                document.append("sterne", resultSet.getString("sterne"));
+                document.append("verpflegung", resultSet.getString("verpflegung"));
                 document.append("anschrift",
                     new Document("ort", resultSet.getString("ort"))
                         .append("plz", resultSet.getString("plz"))
                         .append("strasse", resultSet.getString("strasse"))
                 );
 
-                MongoCollection<Document> collection = this.database.getCollection("Kunde");
-                collection.insertOne(document);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        // migrate to mitarbeiter
-        try (Statement statement = this.connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Mitarbeiter INNER JOIN Person P ON Mitarbeiter.personid = P.id");
-
-            while (resultSet.next()) {
-                Document document = new Document();
-                document.append("steuernummer", resultSet.getString("steuernummer"));
-                document.append("gehalt", resultSet.getString("gehalt"));
-                document.append("name", resultSet.getString("name"));
-                document.append("svnummer", resultSet.getString("SVNummer"));
-                document.append("geburtsdatum", resultSet.getString("geburtsdatum"));
-                document.append("email", resultSet.getString("email"));
-                document.append("anschrift",
-                    new Document("ort", resultSet.getString("ort"))
-                        .append("plz", resultSet.getString("plz"))
-                        .append("strasse", resultSet.getString("strasse"))
-                );
-
-                MongoCollection<Document> collection = this.database.getCollection("Mitarbeiter");
+                MongoCollection<Document> collection = this.database.getCollection("Hotel");
                 collection.insertOne(document);
             }
 
